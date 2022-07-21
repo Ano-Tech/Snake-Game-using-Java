d69fcba424b1e16d347379ecf3e823979debab83
Ano-Tech <63313203+Ano-Tech@users.noreply.github.com>
2022-07-21 13:52:24 +0100

    Add files via upload

---
+++ Food.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.awt.*;
+
+public class Food {
+    private int x;
+    private int y;
+
+    public Food(Snake player) {
+        this.random_spawn(player);
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void random_spawn(Snake player) {
+
+
+       boolean onSnake = true;
+       while(onSnake) {
+           onSnake = false;
+
+           x = (int) (Math.random() * Game.width);
+           y = (int) (Math.random() * Game.height);
+
+           for(Rectangle r : player.getBody()){
+               if(r.x == x && r.y == y) {
+                   onSnake = true;
+               }
+           }
+       }
+    }
+}
